245411c475ea437cd49110e8fb9068c05dd37c4a
Siavash Katebzadeh <mr.katebzadeh.com>
2016-02-22 19:57:17 +0330

    Added distance module.

---
+++ common/distance.py
@@ -0,0 +1,32 @@
+class DistanceCalculator(object):
+    def get_all_distance(self):
+        pass
+
+    def get_distance(self, customer1, customer2):
+        pass
+
+    def __init__(self, customers):
+        self.customers = customers
+
+
+class Floyd(DistanceCalculator):
+    def calculate(self):
+        size = len(self.customers)
+        for k in xrange(size):
+            for i in xrange(size):
+                for j in xrange(size):
+                    tmp = self.customers[i].distance(self.customers[j])
+                    if self.distances[i][j] > tmp:
+                        self.distances[i][j] = tmp
+
+    def get_all_distance(self):
+        return self.distances
+
+    def get_distance(self, customer1_idx, customer2_idx):
+        return self.distances[customer1_idx][customer2_idx]
+
+    def __init__(self, customers):
+        super(Floyd, self).__init__(customers)
+        self.distances = [[999999999 for _ in range(len(customers))] for _ in range(len(customers))]
+
+
