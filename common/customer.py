9c1db4000d2d8fc5674fb549ed41b413d3bcdd59
Siavash Katebzadeh <mr.katebzadeh.com>
2016-02-22 20:57:05 +0330

    Added new constructor.

--- common/customer.py
+++ common/customer.py
@@ -6,8 +6,14 @@ class Customer:
     def __init__(self, idx, x, y, demand):
         (self.idx, self.x, self.y, self.demand) = (idx, x, y, demand)
 
+    def __init__(self, idx, x, y):
+        (self.idx, self.x, self.y, self.demand) = (idx, x, y, 0)
+
     def __str__(self):
         return "Idx: {0}, X: {1}, Y:{2}, Demand: {3}".format(self.idx, self.x, self.y, self.demand)
 
     def distance(self, other):
         return math.sqrt(math.fabs((self.x - other.x) ** 2 + (self.y - other.y) ** 2))
+
+    def set_demand(self, demand):
+        self.demand = demand
