e33b7665b48ce750c8fe4bb8e70460407bb012f3
Siavash Katebzadeh <mr.katebzadeh.com>
2016-02-22 20:28:22 +0330

    Added Euclidean class

--- common/distance.py
+++ common/distance.py
@@ -30,3 +30,23 @@ class Floyd(DistanceCalculator):
         self.distances = [[999999999 for _ in range(len(customers))] for _ in range(len(customers))]
 
 
+class Euclidean(DistanceCalculator):
+    def calculate(self):
+        size = len(self.customers)
+        for i in xrange(size):
+            for j in xrange(size):
+                self.distances[i][j] = self.customers[i].distance(self.customers[j])
+
+    def get_all_distance(self):
+        return self.distances
+
+    def get_distance(self, customer1_idx, customer2_idx):
+        return self.distances[customer1_idx][customer2_idx]
+
+    def __init__(self, customers):
+        super(Floyd, self).__init__(customers)
+        self.distances = [[0 for _ in range(len(customers))] for _ in range(len(customers))]
+
+
+
+
