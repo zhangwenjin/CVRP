c22a6c74c07799a8cb70ab0880a951564d2791c5
Siavash Katebzadeh <mr.katebzadeh.com>
2016-02-27 19:46:18 +0330

    Changed Greedy1 class.

--- greedy/greedy1.py
+++ greedy/greedy1.py
@@ -5,15 +5,12 @@ class Greedy(Algorithm):
     def __init__(self, input_parameter):
         super(Greedy, self).__init__(input_parameter)
         self.C = input_parameter.customers[:]
-        self.depot = input_parameter.depot
-        self.maximum_load = input_parameter.maximum_load
-        self.vehicles_num = input_parameter.vehicles_num
         self.W = []
         self.O = [i + 1 for i in xrange(len(self.customers))]
 
     def run(self):
-        last_point = [0 for i in xrange(2 * self.vehicles_num)]
-        for i in xrange(len(last_point)):
+        self.W = []
+        for i in xrange(2 * self.vehicles_num):
             min_dis = self.C[self.O[0]].distance(self.depot[0], self.depot[1])
             min_node = self.O[0]
             for j in xrange(1, len(self.O) - 1):
@@ -21,8 +18,21 @@ class Greedy(Algorithm):
                 if min_dis > tmp:
                     min_dis = tmp
                     min_node = self.O[j]
-            last_point[i] = min_node
+            self.W.append(min_node)
             self.O.remove(min_node)
+        return self.W
 
+    def run2(self):
+        for j in xrange(len(self.O)):
+            min_dis = self.C[self.O[j]].distance(self.C[self.W[0]][0], self.C[self.W[0]][1])
+            min_node = self.O[0]
+            for w in xrange(len(self.W)):
+                tmp = self.C[self.O[j]].distance(self.C[self.W[w]][0], self.C[self.W[w]][1])
+                if min_dis > tmp:
+                    min_dis = tmp
+                    min_node = w
+            self.W.append(min_node)
+            self.O.remove(min_node)
+        return self.W
 
 
