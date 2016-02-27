91429635450364100d8d8df58b3cde72a5a59e4c
Siavash Katebzadeh <mr.katebzadeh.com>
2016-02-27 10:38:50 +0330

    Added parameter module.

---
+++ common/parameter.py
@@ -0,0 +1,10 @@
+class InputParameter:
+    def __init__(self, customers, depot, vehicles_num, maximum_load):
+        (self.customers, self.depot, self.vehicles_num, self.vehicles_num) \
+            = (customers, depot, vehicles_num, maximum_load)
+
+
+class OutputParameter:
+    def __init__(self, paths, distance):
+        (self.paths, self.distance) = (paths, distance)
+
