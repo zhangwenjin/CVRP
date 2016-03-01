2b38277d3e9f6ac0283d7f8a6a1e8be14f865c5a
Siavash Katebzadeh <mr.katebzadeh.com>
2016-03-01 18:27:56 +0330

    Fixed depot position.

--- greedy/greedy2.py
+++ greedy/greedy2.py
@@ -1,5 +1,6 @@
 from common.algorithm import Algorithm
-from math import sqrt, fabs
+from common.distance import distance
+
 
 class Greedy2(Algorithm):
     def __init__(self, input_parameter):
@@ -22,7 +23,7 @@ class Greedy2(Algorithm):
         min_distance = 10000
         min_node = -1
 
-        for c in xrange(len(self.customers)):
+        for c in xrange(1, len(self.customers)):
             tmp = self.customers[c].distance(location[0], location[1])
             if min_distance > tmp and self.customers[c].demand <= demand:
                 min_distance = tmp
@@ -30,18 +31,33 @@ class Greedy2(Algorithm):
         return min_node
 
     def run(self):
+        min_demand = min([c.demand for c in self.customers[1:]])
+        s = 0
+        locations = [self.depot for _ in xrange(self.vehicles_num)]
+        current_loads = [self.maximum_load for _ in xrange(self.vehicles_num)]
+        paths = [[1] for _ in xrange(self.vehicles_num)]
+        sum_distances = [0 for _ in xrange(self.vehicles_num)]
+        # for _ in xrange(1):
+        #     for i in xrange(self.vehicles_num):
+        #         customer = self.find_nearest(locations[i], current_loads[i])
+        #         if customer == -1:
+        #             continue
+        #         paths[i].append(self.customers[customer].idx)
+        #         current_loads[i] -= self.customers[customer].demand
+        #         sum_distances[i] += self.customers[customer].distance(locations[i][0], locations[i][1])
+        #         locations[i] = (self.customers[customer].x, self.customers[customer].y)
+        #         del self.customers[customer]
+
         for i in xrange(self.vehicles_num):
-            location = self.depot
-            current_load = self.maximum_load
-            path = [0]
-            sum_distance = 0
+            location = locations[i]
+            current_load = current_loads[i]
+            path = paths[i]
+            sum_distance = sum_distances[i]
             while True:
-                if current_load == 0:
-                    break
                 customer = self.find_nearest(location, current_load)
-                if customer == -1:
-                    path.append(0)
-                    sum_distance += sqrt(fabs((location[0] - self.depot[0])**2 - (location[1] - self.depot[1])**2))
+                if customer == -1 or current_load == 0:
+                    path.append(1)
+                    sum_distance += distance(location, self.depot)
                     break
                 path.append(self.customers[customer].idx)
                 current_load -= self.customers[customer].demand
@@ -49,3 +65,5 @@ class Greedy2(Algorithm):
                 location = (self.customers[customer].x, self.customers[customer].y)
                 del self.customers[customer]
             print path, sum_distance
+            s += sum_distance
+        print 'Sum is :', s
