827dee3610cbb05f4ffdbf15ba48d3fd42d8da13
Siavash Katebzadeh <mr.katebzadeh.com>
2016-03-01 18:26:39 +0330

    Added distance method.

--- common/distance.py
+++ common/distance.py
@@ -1,3 +1,6 @@
+from math import sqrt, fabs
+
+
 class DistanceCalculator(object):
     def get_all_distance(self):
         pass
@@ -30,12 +33,17 @@ class Floyd(DistanceCalculator):
         self.distances = [[999999999 for _ in range(len(customers))] for _ in range(len(customers))]
 
 
+def distance(point1, point2):
+        return (sqrt(fabs((point1[0] - point2[0])**2 +
+                         (point1[1] - point2[1])**2)))
+
+
 class Euclidean(DistanceCalculator):
     def calculate(self):
         size = len(self.customers)
         for i in xrange(size):
             for j in xrange(size):
-                self.distances[i][j] = self.customers[i].distance(self.customers[j])
+                self.distances[i][j] = self.customers[i].distance2(self.customers[j])
 
     def get_all_distance(self):
         return self.distances
@@ -44,9 +52,7 @@ class Euclidean(DistanceCalculator):
         return self.distances[customer1_idx][customer2_idx]
 
     def __init__(self, customers):
-        super(Floyd, self).__init__(customers)
+        super(Euclidean, self).__init__(customers)
         self.distances = [[0 for _ in range(len(customers))] for _ in range(len(customers))]
-
-
 
 
